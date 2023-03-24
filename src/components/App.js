5c16285193d4a38bc470e3efc1aaf5cede651b68
stanken_a <artyemst3@mail.ru>
2023-03-24 15:31:39 +0300

    feat: add new header styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import '../index.css';
 import Header from './Header';
 import Main from './Main';
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,18 @@ import logoImg from '../images/logo.svg';
 export default function Header() {
   return (
     <header className="header">
-      <a href="#" className="header__link">
-        <img src={logoImg} alt="Логотип Mesto" className="header__logo" />
-      </a>
+      <div className="header__container">
+        <div className="header__logo-container">
+          <a href="#" className="header__link">
+            <img src={logoImg} alt="Логотип Mesto" className="header__logo" />
+          </a>
+          <button type="button" className="header__btn"></button>
+        </div>
+        <div className="header__user">
+          <a href="#" className="header__mail">email@mail.com</a>
+          <button type="button" className="header__logout">Выйти</button>
+        </div>
+      </div>
     </header>
   )
 }
