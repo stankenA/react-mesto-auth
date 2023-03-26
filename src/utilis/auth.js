d07d68875886346129c2ad161dc5033265079329
stanken_a <artyemst3@mail.ru>
2023-03-26 13:33:33 +0300

    fix: optimize auth.js logic

--- src/utilis/auth.js
+++ src/utilis/auth.js
@@ -8,47 +8,46 @@ function checkResponse(res) {
   return Promise.reject(`Что-то пошло не так: ${res.status}`);
 }
 
+function request(endpoint, options) {
+  return fetch(`${BASE_URL + endpoint}`, options).then(checkResponse)
+}
+
 export const register = (password, email) => {
-  return fetch(`${BASE_URL}/signup`, {
+  return request(`/signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ password, email })
   })
-    .then((response) => checkResponse(response))
     .then((res) => {
       return res;
     })
-    .catch((err) => console.log(err));
 };
 
 export const authorize = (password, email) => {
-  return fetch(`${BASE_URL}/signin`, {
+  return request(`/signin`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ password, email })
   })
-    .then((res => checkResponse(res)))
     .then((data) => {
       if (data.token) {
         localStorage.setItem('jwt', data.token);
         return data;
       }
     })
-    .catch(err => console.log(err))
 };
 
 export const checkToken = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
+  return request(`/users/me`, {
     method: 'GET',
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${token}`
     }
   })
-    .then(res => checkResponse(res))
     .then(data => data)
 }
