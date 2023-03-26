f86e57c1e0d8cdaecc9b4b788815129e6742baa5
stanken_a <artyemst3@mail.ru>
2023-03-26 13:42:11 +0300

    fix: replace AddPlacePopup useRef logic with controlled components

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,22 +1,30 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
 
 export default function AddPlacePopup({ isOpen, onClose, onAddPlace, isLoading, onBgClose }) {
 
-  const nameRef = useRef();
-  const linkRef = useRef();
+  const [nameValue, setNameValue] = useState('');
+  const [linkValue, setLinkValue] = useState('');
 
   useEffect(() => {
-    nameRef.current.value = '';
-    linkRef.current.value = '';
+    setNameValue('');
+    setLinkValue('');
   }, [isOpen]);
+
+  function handleNameChange(evt) {
+    setNameValue(evt.target.value);
+  }
+
+  function handleLinkChange(evt) {
+    setLinkValue(evt.target.value);
+  }
 
   function handleSubmit(evt) {
     evt.preventDefault();
 
     onAddPlace({
-      name: nameRef.current.value,
-      link: linkRef.current.value
+      name: nameValue,
+      link: linkValue
     });
   }
 
@@ -30,11 +38,30 @@ export default function AddPlacePopup({ isOpen, onClose, onAddPlace, isLoading, 
       submitButtonText={isLoading ? 'Сохранение...' : 'Сохранить'}
       onBgClose={onBgClose}
     >
-      <input type="text" id="title-input" className="popup__input popup__input_type_photo-title" placeholder="Название"
-        name="photoTitle" required minLength="2" maxLength="30" autoComplete="off" ref={nameRef} />
+      <input
+        type="text"
+        id="title-input"
+        className="popup__input popup__input_type_photo-title"
+        placeholder="Название"
+        name="photoTitle"
+        required minLength="2"
+        maxLength="30"
+        autoComplete="off"
+        onChange={handleNameChange}
+        value={nameValue}
+      />
       <span className="popup__input-error title-input-error"></span>
-      <input type="url" id="href-input" className="popup__input popup__input_type_photo-href"
-        placeholder="Ссылка на картинку" name="photoHref" required autoComplete="off" ref={linkRef} />
+      <input
+        type="url"
+        id="href-input"
+        className="popup__input popup__input_type_photo-href"
+        placeholder="Ссылка на картинку"
+        name="photoHref"
+        required
+        autoComplete="off"
+        onChange={handleLinkChange}
+        value={linkValue}
+      />
       <span className="popup__input-error href-input-error"></span>
     </PopupWithForm>
   )
